6086e5a86344fb2c7cebfd3d8c1cb0b698850be3
Adi Murgescu <murgescuadi@gmail.com>
2024-09-27 11:44:22 -0400

    arrayToString function with main that test

---
+++ 2024-09-27/arraysClasswork.java
@@ -0,0 +1,24 @@
+public class arraysClasswork{
+  public static String arrayToString(int[] nums){
+    String out = "[";
+    for (int i = 0; i < nums.length; i++){
+      out += nums[i];
+      if (i < nums.length - 1){
+        out += ", ";
+      }
+    }
+    return out + "]";
+  }
+
+
+
+
+  public static void main (String[] args){
+    int [] test = {1,4,5,9};
+    System.out.println(arrayToString(test));
+    int [] test1 = {1};
+    System.out.println(arrayToString(test1));
+    int [] test2 = {};
+    System.out.println(arrayToString(test2));
+  }
+}
