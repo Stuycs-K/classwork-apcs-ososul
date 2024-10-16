c37825a5d315d25eb81ecc92f73f4eb8319dcbe9
Adi <murgescuadi@gmail.com>
2024-10-16 10:49:58 -0400

    added test cases and tested methods

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -9,13 +9,23 @@ public class Demo{
   }
 
   public static void main(String[]args){
-    Point p1 = new Point(1,1);
-    Point p2 = new Point(-1,-1);
-    Point p3 = new Point(3,4);
-    Point test = new Point(0,0);
-    System.out.println( p3);
-    System.out.println( distance(p1,p2));
-    System.out.println( Point.distance(p1,p2));
-    System.out.println( p1.distanceTo(p2));
+    Point p1 = new Point(1, 1);
+        Point p2 = new Point(-1, -1);
+        Point p3 = new Point(3, 4);
+        Point p4 = new Point(0, 0);
+
+        System.out.println(Point.distance(p1, p2));
+        System.out.println(p1.distanceTo(p2));
+        System.out.println(Point.distance(p3, p4));
+        System.out.println(p1.distanceTo(p1));
+
+        // Equilateral Triangle
+        Point pA = new Point(0, 0);
+        Point pB = new Point(1, 0);
+        Point pC = new Point(0.5, Math.sqrt(3) / 2);
+
+        System.out.println(Point.distance(pA, pB));
+        System.out.println(Point.distance(pB, pC));
+        System.out.println(Point.distance(pC, pA));
   }
 }
