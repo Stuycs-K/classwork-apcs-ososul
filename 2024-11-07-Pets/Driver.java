4615b049997e5934de26aa2b816f96469931b7ac
Adi <murgescuadi@gmail.com>
2024-11-07 22:30:31 -0500

    updating work from class

---
+++ 2024-11-07-Pets/Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+
+    public static void main (String[] args){
+      Animal a = new Animal("Hello", 78, "jeremy");
+      a.speak();
+      Animal b = new Bird("chirp", 10, "bob", 30.5, "blue");
+      b.speak();
+  
+    }
+  
+  
+  }
