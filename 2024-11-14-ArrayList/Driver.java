3579d9a43cd5f3f88b503a7bbb782cd6edd5f29a
Adi <murgescuadi@gmail.com>
2024-11-18 10:56:58 -0500

    successfully tested methods

--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -2,8 +2,27 @@ import java.util.ArrayList;
 public class Driver {
 
   public static void main (String[] args){
-    System.out.println(ArrayListPractice.createRandomArray(5));
-    //System.out.println(ArrayListPractice.createRandomArray(200000));
+    ArrayList<String> randomList = ArrayListPractice.createRandomArray(5);
+        System.out.println("Random List: " + randomList);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("");
+        list.add("2");
+        list.add("");
+        ArrayListPractice.replaceEmpty(list);
+        System.out.println("After replaceEmpty: " + list);
+
+        ArrayList<String> reversed = ArrayListPractice.makeReversedList(list);
+        System.out.println("Reversed List: " + reversed);
+
+        ArrayList<String> a = new ArrayList<>();
+        a.add("1");
+        a.add("3");
+        ArrayList<String> b = new ArrayList<>();
+        b.add("2");
+        b.add("4");
+        ArrayList<String> mixed = ArrayListPractice.mixLists(a, b);
+        System.out.println("Mixed List: " + mixed);
   }
 
 
