1e37c41d5d36889db59b5b06164c551f72037247
Adi Murgescu <murgescuadi@gmail.com>
2024-11-19 11:39:54 -0500

    parsing file

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -1,8 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Day1 {
+
+  public static String [] toArray (String filename){
+    try {
+        Scanner sc = new Scanner(new File(filename));
+        String [] output = sc.split(", ");
+        return output;
+
+        }
+        sc.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("Error: File not found");
+    }
+  }
 
 
   public static void main(String[] args) {
     String file = "input.txt";
+    String [] dataArray = toArray(file);
 
   }
 }
