91d5bbb387e8828b317caa7bc0083847d33d2c61
Adi <murgescuadi@gmail.com>
2024-11-19 19:35:04 -0500

    wrote main function and solved problem using main

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -13,21 +13,53 @@ public class Day1 {
             String line = scan.nextLine();
             output = line.split(", ");
         }
-        scanner.close();
+        scan.close();
     } catch (FileNotFoundException e) {
         System.out.println("Error: File not found");
     }
     return output;
 }
 
-}
 
-  public static 
+
+public static int calculateDistance(String[] data) {
+  int[][] directions = new int[4][2];
+  directions[0][0] = 0; directions[0][1] = 1; // North
+  directions[1][0] = 1; directions[1][1] = 0; // East
+  directions[2][0] = 0; directions[2][1] = -1; // South
+  directions[3][0] = -1; directions[3][1] = 0; // West
+
+  int x = 0;
+  int y = 0;
+  int currentDirection = 0;
+
+  for (int i = 0; i < data.length; i++) {
+      String step = data[i];
+      char turn = step.charAt(0);
+      int distance = Integer.parseInt(step.substring(1));
+
+      if (turn == 'L') {
+          currentDirection = (currentDirection + 3) % 4;
+      } else if (turn == 'R') {
+          currentDirection = (currentDirection + 1) % 4;
+      }
+
+      x = x + directions[currentDirection][0] * distance;
+      y = y + directions[currentDirection][1] * distance;
+  }
+
+  return Math.abs(x) + Math.abs(y);
+} 
 
 
   public static void main (String[] args) {
     String file = "input.txt";
     String [] dataArray = toArray(file);
+    int x = calculateDistance(dataArray);
+    System.out.println(x);
 
   }
+
+
 }
+
