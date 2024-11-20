dc7166dda4a669dbb3454f422df915edc5c2bf7c
Adi <murgescuadi@gmail.com>
2024-11-19 20:10:27 -0500

    wrote function to solve after parsing

--- 2024-11-18-advent2016/Day2.java
+++ 2024-11-18-advent2016/Day2.java
@@ -22,6 +22,47 @@ public class Day2 {
         return output;
     }
 
+    public static String findBathroomCode(String[] instructions) {
+        int[][] keypad = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int row = 1; // Starting at 5
+        int col = 1;
+        String code = "";
+
+        for (int i = 0; i < instructions.length; i++) {
+            String line = instructions[i];
+            for (int j = 0; j < line.length(); j++) {
+                char move = line.charAt(j);
+
+                if (move == 'U') {
+                    if (row > 0) {
+                        row = row - 1;
+                    }
+                } else if (move == 'D') {
+                    if (row < 2) {
+                        row = row + 1;
+                    }
+                } else if (move == 'L') {
+                    if (col > 0) {
+                        col = col - 1;
+                    }
+                } else if (move == 'R') {
+                    if (col < 2) {
+                        col = col + 1;
+                    }
+                }
+            }
+            int number = keypad[row][col];
+            code = code + number;
+        }
+
+        return code;
+    }
+
 
 
 
