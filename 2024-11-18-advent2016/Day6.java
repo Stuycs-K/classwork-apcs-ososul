2266b812df378c90e6e85d6c97f0341c76082dd0
Adi <murgescuadi@gmail.com>
2024-11-21 12:30:48 -0500

    finished the method that solves problem; correctly solved and fixed main to do so

--- 2024-11-18-advent2016/Day6.java
+++ 2024-11-18-advent2016/Day6.java
@@ -22,14 +22,43 @@ public class Day6 {
     }
 
     public static String message(String[] m){
-        
+        if (m.length == 0) {
+            return "";
+        }
+
+        int length = m[0].length();
+        String result = "";
+
+        // make array of the freqs
+        for (int i = 0; i < length; i++) {
+            int[] freq = new int[26]; 
+
+            for (int j = 0; j < m.length; j++) {
+                char ch = m[j].charAt(i);
+                freq[ch - 'a']++;
+            }
+
+            int maxFreq = 0;
+            char mostCommon = ' ';
+            for (int k = 0; k < 26; k++) {
+                if (freq[k] > maxFreq) {
+                    maxFreq = freq[k];
+                    mostCommon = (char) (k + 'a');
+                }
+            }
+
+            result = result + mostCommon;
+        }
+
+        return result;
     }
+    
 
 
     public static void main(String[] args) {
         String fileName = "input.txt";
         String[] outAry = getInput(fileName);
-        int sum = message(outAry);
-        System.out.println(sum);
+        String answer = message(outAry);
+        System.out.println(answer);
     }
 }
