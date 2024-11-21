db9f0def80fffbf98448e10dd2b8f744e70aa2a7
Adi <murgescuadi@gmail.com>
2024-11-21 12:22:19 -0500

    copied fileReading code, set up methods, and wrote main

--- 2024-11-18-advent2016/Day6.java
+++ 2024-11-18-advent2016/Day6.java
@@ -1,10 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Day6 {
-    
+    public static String[] getInput(String fileName) {
+        String[] output = new String[0];
+        try {
+            File file = new File(fileName);
+            Scanner scan = new Scanner(file);
+            String total = "";
+            while (scan.hasNextLine()) {
+                String line = scan.nextLine();
+                total = total + line + "\n";
+            }
+            output = total.split("\n");
+            scan.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found.");
+        }
+        return output;
+    }
 
-
+    public static String message(String[] m){
+        
+    }
 
 
     public static void main(String[] args) {
-        
+        String fileName = "input.txt";
+        String[] outAry = getInput(fileName);
+        int sum = message(outAry);
+        System.out.println(sum);
     }
 }
