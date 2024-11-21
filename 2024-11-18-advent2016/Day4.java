0fa73da06da7b457184aa9e74de961836e16f356
Adi <murgescuadi@gmail.com>
2024-11-21 12:12:19 -0500

    wrote working code for getSum method and solved (finally)

--- 2024-11-18-advent2016/Day4.java
+++ 2024-11-18-advent2016/Day4.java
@@ -23,11 +23,56 @@ public class Day4 {
     }
     
     public static int getSum(String[] rooms) {
-
-        return 0;
+        int sum = 0;
+    
+        for (int i = 0; i < rooms.length; i++) {
+            String room = rooms[i];
+            int dash = room.lastIndexOf("-");
+            int open = room.indexOf("[");
+            int close = room.indexOf("]");
+    
+            String name = room.substring(0, dash).replace("-", "");
+            int id = Integer.parseInt(room.substring(dash + 1, open));
+            String check = room.substring(open + 1, close);
+    
+            // count freq
+            int[] freq = new int[26];
+            for (int j = 0; j < name.length(); j++) {
+                char ch = name.charAt(j);
+                freq[ch - 'a']++;
+            }
+    
+            // put into array
+            char[] letters = new char[26];
+            for (int j = 0; j < 26; j++) {
+                letters[j] = (char) ('a' + j);
+            }
+    
+            // sort
+            for (int j = 0; j < 25; j++) {
+                for (int k = j + 1; k < 26; k++) {
+                    if (freq[letters[j] - 'a'] < freq[letters[k] - 'a'] ||
+                        (freq[letters[j] - 'a'] == freq[letters[k] - 'a'] && letters[j] > letters[k])) {
+                        char temp = letters[j];
+                        letters[j] = letters[k];
+                        letters[k] = temp;
+                    }
+                }
+            }
+    
+            String calc = "";
+            for (int j = 0; j < 5; j++) {
+                calc = calc + letters[j];
+            }
+    
+            if (calc.equals(check)) {
+                sum = sum + id;
+            }
+        }
+    
+        return sum;
     }
-
-
+    
     public static void main(String[] args) {
         String fileName = "input.txt";
         String[] outAry = getInput(fileName);
