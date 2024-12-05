00d4d852a1036e3a850aedf05a0ede539e37397e
Adi <murgescuadi@gmail.com>
2024-12-04 23:05:02 -0500

    finished color demo

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,15 +1,37 @@
+import java.util.Random;
+
 public class ColorDemo {
     public static void main(String[] args) {
-        // Clear screen and move cursor to top-left
+        
         System.out.print("\u001b[2J\u001b[H");
 
-        // Print colorful text
-        System.out.print("\u001b[31mRed Text ");
-        System.out.print("\u001b[32mGreen Text ");
-        System.out.print("\u001b[34mBlue Text ");
-        System.out.println("\u001b[33mYellow Text");
+        System.out.print("\u001b[31mRed hello ");
+        System.out.print("\u001b[32mGreen hello ");
+        System.out.print("\u001b[34mBlue hello ");
+        System.out.println("\u001b[33mYellow hello");
 
-        // Reset terminal to defaults
+        for (int row = 0; row < 35; row++) {
+            if (row % 2 == 0) {
+                System.out.print("\u001b[41m");
+            } else {
+                System.out.print("\u001b[44m");
+            }
+            for (int col = 0; col < 100; col++) {
+                System.out.print("  ");
+            }
+            System.out.println("\u001b[0m");
+        }
+
+        Random rand = new Random();
+
+        for (int row = 0; row < 20; row++) {
+            for (int col = 0; col < 40; col++) {
+                int color = 41 + rand.nextInt(6); 
+                System.out.print("\u001b[" + color + "m  ");
+            }
+            System.out.println("\u001b[0m"); 
+        }
+
         System.out.print("\u001b[0m");
     }
 }
