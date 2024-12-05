02889a906f3ab3cb9712319779c5803c7055b8af
Adi <murgescuadi@gmail.com>
2024-12-05 10:54:42 -0500

    rewrote to use methods, not just in my main

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -9,7 +9,18 @@ public class Driver {
         int width = 80;
         int height = 30;
 
-        // Border
+
+        drawBorder(width, height); // Border
+        int[] randomNumbers = generateRandomIntegers(); // Random 
+        displayRandomIntegers(randomNumbers, width); // Display 
+        drawSeparator(3, width); //  separator
+        drawCenterMessage(width, height); // Center 
+
+        Text.go(31, 1);
+        System.out.println(Text.RESET);
+    }
+
+    private static void drawBorder(int width, int height) {
         for (int col = 1; col <= width; col++) { // Top 
             Text.go(1, col);
             Text.color(Text.BRIGHT, Text.RED);
@@ -28,41 +39,46 @@ public class Driver {
             Text.go(row, width);
             System.out.print("#");
         }
+    }
 
-        // random
+    private static int[] generateRandomIntegers() {
         Random rand = new Random();
         int[] randomNumbers = new int[3];
         randomNumbers[0] = rand.nextInt(100);
         randomNumbers[1] = rand.nextInt(100);
         randomNumbers[2] = rand.nextInt(100);
+        return randomNumbers;
+    }
 
+    private static void displayRandomIntegers(int[] numbers, int width) {
         int spacing = width / 4;
         for (int i = 0; i < 3; i++) {
             int col = (i + 1) * spacing;
             Text.go(2, col);
-            if (randomNumbers[i] < 25) {
+            if (numbers[i] < 25) {
                 Text.color(Text.BRIGHT, Text.RED);
-            } else if (randomNumbers[i] > 75) {
+            } else if (numbers[i] > 75) {
                 Text.color(Text.BRIGHT, Text.GREEN);
             } else {
                 Text.color(Text.BRIGHT, Text.WHITE);
             }
-            System.out.print(randomNumbers[i]);
+            System.out.print(numbers[i]);
         }
+    }
 
+    private static void drawSeparator(int row, int width) {
         Text.go(3, 1);
         Text.color(Text.BRIGHT, Text.YELLOW);
         for (int i = 0; i < width; i++) {
             System.out.print("-");
         }
+    }
 
+    private static void drawCenterMessage(int width, int height) {
         int centerX = width / 2;
         int centerY = height / 2;
         Text.go(centerY, centerX - 10); // Adjusted for length
         Text.color(Text.BRIGHT, Text.MAGENTA);
         System.out.print("Hello this is the center :>");
-
-        Text.go(31, 1);
-        System.out.println(Text.RESET);
     }
 }
