3c2b697b03f15b22e727be812bb39d1fed028a85
Adi <murgescuadi@gmail.com>
2024-12-15 17:17:36 -0500

    wrote driver to test subclass

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,3 +1,22 @@
 public class Driver {
-  
+    public static void main(String[] args) {
+        Adventurer wizard1 = new Wizard("Jimmy");
+        Adventurer wizard2 = new Wizard("Gary");
+
+        System.out.println(wizard1.attack(wizard2));
+        System.out.println(wizard2.getName() + " has " + wizard2.getHP() + " HP remaining.");
+
+        System.out.println(wizard1.support(wizard2));
+        System.out.println(wizard2.getName() + " has " + wizard2.getHP() + " HP.");
+
+        System.out.println(wizard1.support());
+        System.out.println(wizard1.getName() + " has " + wizard1.getHP() + " HP.");
+
+        System.out.println(wizard1.specialAttack(wizard2));
+        System.out.println(wizard2.getName() + " has " + wizard2.getHP() + " HP.");
+
+        System.out.println(wizard1.specialAttack(wizard2));
+        System.out.println(wizard1.getName() + " has " + ((Wizard) wizard1).getSpecial() + " Energy.");
+    }
 }
+
