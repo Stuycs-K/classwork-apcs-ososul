519d41c0170a03e4041e2f16df163a5fe28267e4
Adi Murgescu <murgescuadi@gmail.com>
2025-01-02 11:43:22 -0500

    code

--- 2025-01-02/Fibonacci.java
+++ 2025-01-02/Fibonacci.java
@@ -7,4 +7,22 @@ public class Fibonacci {
       return (fib(n-2) + fib(n-1));
     }
   }
+
+  public static void main(String[] args) {
+    int k = 47;
+    for (int i = 0; i <= k; i++){
+      System.out.println(fib(i));
+    }
+  }
+
+  // overflows at 47, doesn't at 46 //
+
+
+
+
+
+
+
+
+
 }
